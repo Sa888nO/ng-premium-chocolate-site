653c82547385c0431c3a7dabb8f131c96af55bf0
Sa888nO <shura.reno.97@bk.ru>
2022-09-08 17:08:48 +0300

    done routing of the project
    1) Добавил r-r-d v6 в проект
    2) Реализовал роутинг проекта

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,8 +1,26 @@
-import "./App.css";
-import H from "@components/H";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-  return <H />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>main</div>} />
+        <Route path="/shop" element={<div>shop</div>} />
+        <Route path="/corporate-orders" element={<div>corporate-orders</div>} />
+        <Route
+          path="/delivery-and-payment"
+          element={<div>delivery-and-payment</div>}
+        />
+        <Route path="/about-author" element={<div>about-author</div>} />
+        <Route
+          path="/composition-and-storage"
+          element={<div>composition-and-storage</div>}
+        />
+        <Route path={"/contacts"} element={<div>contacts</div>} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- frontend/src/components/H/H.jsx
+++
@@ -1,8 +0,0 @@
-import classNames from "classnames";
-
-import styles from "./H.module.scss";
-
-const H = () => {
-  return <div className={classNames(styles.font)}>1</div>;
-};
-export default H;

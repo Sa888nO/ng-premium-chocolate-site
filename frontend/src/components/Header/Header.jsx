daaa0d679374b8bc13206bfc1de3a18c159510b3
Sa888nO <shura.reno.97@bk.ru>
2022-09-11 15:55:33 +0300

    made burger button and hiding menu

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,37 +1,54 @@
+/* eslint-disable no-console */
+import { useState } from "react";
+
 import classNames from "classnames";
 import { Link } from "react-router-dom";
 
 import SocialLinksBlock from "./components/SocialLinksBlock";
 import styles from "./Header.module.scss";
 
-const Header = () => {
+const Header = (activPage) => {
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <div>
-      <input type="checkbox" id="checkbox" onClick={() => {}}>
-        <div className={classNames(styles.header)}>
-          <div className={classNames(styles.block_title)}>
-            <div>
-              <SocialLinksBlock />
-            </div>
-            <h1>
-              <Link to={"/"}>Monomo</Link>
-            </h1>
-            <span>hello@ng.com</span>
-          </div>
-          <div className={classNames(styles.block_link)}>
-            <Link to={"/shop"}>Каталог</Link>
-            <Link to={"/corporate-orders"}>Корпоративные заказы</Link>
-            <Link to={"/delivery-and-payment"}>Доставка и оплата</Link>
-            <Link to={"/about-author"}>Об авторе</Link>
-            <Link to={"/composition-and-storage"}>Хранение</Link>
-            <Link to={"/contacts"}>Контакты</Link>
-          </div>
-          <div className={classNames(styles["hidden-block"])}>
+      <div className={classNames(styles["button-block"])}>
+        <button
+          className={classNames(
+            styles["c-hamburger"],
+            styles["c-hamburger--htx"],
+            { [styles["is-active"]]: isOpen }
+          )}
+          onClick={() => {
+            setOpen(!isOpen);
+          }}
+        >
+          <span></span>
+        </button>
+      </div>
+      <div className={classNames(styles.header, { [styles.a]: !isOpen })}>
+        <div className={classNames(styles.block_title)}>
+          <div>
             <SocialLinksBlock />
-            <span>hello@ng.com</span>
           </div>
+          <h1>
+            <Link to={"/"}>Monomo</Link>
+          </h1>
+          <span>hello@ng.com</span>
         </div>
-      </input>
+        <div className={classNames(styles.block_link)}>
+          <Link to={"/shop"}>Каталог</Link>
+          <Link to={"/corporate-orders"}>Корпоративные заказы</Link>
+          <Link to={"/delivery-and-payment"}>Доставка и оплата</Link>
+          <Link to={"/about-author"}>Об авторе</Link>
+          <Link to={"/composition-and-storage"}>Хранение</Link>
+          <Link to={"/contacts"}>Контакты</Link>
+        </div>
+        <div className={classNames(styles["hidden-block"])}>
+          <SocialLinksBlock />
+          <span>hello@ng.com</span>
+        </div>
+      </div>
     </div>
   );
 };

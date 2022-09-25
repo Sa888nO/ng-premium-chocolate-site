49cfbc0e359e0735a0635bd8a1132276e20fdc68
Sa888nO <shura.reno.97@bk.ru>
2022-09-25 22:43:01 +0300

    created bodycandyItem

--- frontend/src/App/pages/ShopPage/components/BodyCandy/BodyCandy.jsx
+++ frontend/src/App/pages/ShopPage/components/BodyCandy/BodyCandy.jsx
@@ -1,17 +1,20 @@
 import { BodyCandyItem } from "../index";
 import styles from "./BodyCandy.module.scss";
+import { CANDYS } from "./candys";
 
 const BodyCandy = () => {
   return (
     <div className={styles.content}>
-      <BodyCandyItem />
-      <BodyCandyItem />
-      <BodyCandyItem />
-      <BodyCandyItem />
-      <BodyCandyItem />
-      <BodyCandyItem />
-      <BodyCandyItem />
-      <BodyCandyItem />
+      {CANDYS.map((candy, key) => (
+        <BodyCandyItem
+          key={key}
+          image={candy.image}
+          Title={candy.title}
+          subTitle={candy.subTitle}
+          info={candy.info}
+          price={candy.price}
+        />
+      ))}
     </div>
   );
 };
--- frontend/src/App/pages/ShopPage/components/BodyCandyItem/BodyCandyItem.jsx
+++ frontend/src/App/pages/ShopPage/components/BodyCandyItem/BodyCandyItem.jsx
@@ -1,6 +1,43 @@
+import { useState } from "react";
+
+import classNames from "classnames";
+
 import styles from "./BodyCandyItem.module.scss";
 
-const BodyCandyItem = () => {
-  return <div className={styles["candy-card"]}></div>;
+const BodyCandyItem = ({ Title, subTitle, image, info, price }) => {
+  const [currentPrice, setPrice] = useState(price.first);
+
+  return (
+    <div className={styles["candy-card"]}>
+      <div className={styles["candy-card_img"]}>
+        <img src={image} alt="candy" width={200} />
+      </div>
+      <div className={styles["candy-card_title"]}>{Title}</div>
+      <div className={styles["candy-card_sub-title"]}>{subTitle}</div>
+      <div className={styles["candy-card_info"]}>Состав: {info}</div>
+      <div className={styles["candy-card_button-block"]}>
+        Коробочка
+        <button
+          onClick={() => setPrice(price.first)}
+          className={classNames(styles["candy-card_button"], {
+            [styles["candy-card_button_active"]]: currentPrice === price.first,
+          })}
+        >
+          5 шт
+        </button>
+        <button
+          onClick={() => setPrice(price.second)}
+          className={classNames(styles["candy-card_button"], {
+            [styles["candy-card_button_active"]]: currentPrice === price.second,
+          })}
+        >
+          16 шт
+        </button>
+      </div>
+      <div className={styles["candy-card_price"]}>
+        Цена: {currentPrice} руб.
+      </div>
+    </div>
+  );
 };
 export default BodyCandyItem;

2b0928e8422f340142efb0466d5dd881257ac1a9
Sa888nO <shura.reno.97@bk.ru>
2022-10-09 23:48:32 +0300

    fix shop-item component

--- frontend/src/App/pages/ShopPage/ShopPageItem/ShopPageItem.jsx
+++ frontend/src/App/pages/ShopPage/ShopPageItem/ShopPageItem.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import CurrentCandy from "@store/CurrentCandy";
 import classNames from "classnames";
 import { observer } from "mobx-react-lite";
@@ -15,77 +13,21 @@ const price = {
   number30: "3000",
 };
 
-const ShopPageItem = ({ id, title, image, type = true, typeSize = false }) => {
-  const [candyNumber, setCandyNumber] = useState(9);
+const ShopPageItem = ({ id, title, image }) => {
   return (
-    <div className={classNames(styles.card, { [styles.cpec]: typeSize })}>
-      <img className={styles.img} src={image} alt="candy" />
-      <div
-        className={classNames(styles["info-block"], {
-          [styles["info-block-cpec"]]: typeSize,
-        })}
-      >
-        <div className={styles.title}>{title}</div>
-        {/* <div className={styles["price-block"]}>
-          <div className={styles["switch-block"]}>
-            <button
-              onClick={() => setCandyNumber(9)}
-              className={classNames(styles["button-switch"], {
-                [styles["button-switch_active"]]: candyNumber === 9,
-              })}
-            >
-              9 шт
-            </button>
-            <button
-              onClick={() => setCandyNumber(12)}
-              className={classNames(styles["button-switch"], {
-                [styles["button-switch_active"]]: candyNumber === 12,
-              })}
-            >
-              12 шт
-            </button>
-            <button
-              onClick={() => setCandyNumber(16)}
-              className={classNames(styles["button-switch"], {
-                [styles["button-switch_active"]]: candyNumber === 16,
-              })}
-            >
-              16 шт
-            </button>
-            <button
-              onClick={() => setCandyNumber(24)}
-              className={classNames(styles["button-switch"], {
-                [styles["button-switch_active"]]: candyNumber === 24,
-              })}
-            >
-              24 шт
-            </button>
-            <button
-              onClick={() => setCandyNumber(30)}
-              className={classNames(styles["button-switch"], {
-                [styles["button-switch_active"]]: candyNumber === 30,
-              })}
-            >
-              30 шт
-            </button>
-          </div>
-          <div className={styles.price}>
-            Цена: {price[`number${candyNumber}`]} р.
-          </div>
-        </div> */}
-        {type ? (
-          <Link
-            onClick={() => {
-              CurrentCandy.setCurrentCandy(id);
-            }}
-            to={"/tastes"}
-            className={styles.button}
-          >
-            Подробнее
-          </Link>
-        ) : (
-          <></>
-        )}
+    <div className={classNames(styles.card)}>
+      <img className={styles["card__img"]} src={image} alt="candy" />
+      <div className={classNames(styles["card__info-block"])}>
+        <div className={styles["card__title"]}>{title}</div>
+        <Link
+          onClick={() => {
+            CurrentCandy.setCurrentCandy(id);
+          }}
+          to={"/tastes"}
+          className={styles["card__button"]}
+        >
+          Подробнее
+        </Link>
       </div>
     </div>
   );
